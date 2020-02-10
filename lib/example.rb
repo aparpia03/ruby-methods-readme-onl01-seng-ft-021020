f48f347d94ed1e256206d11547f0b7ebd030445f
Abbas Parpia <aparpia03@gmail.com>
2020-02-10 18:48:59 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -7,9 +7,8 @@ def say_hello_world_ten_times
     puts phrase
     puts phrase
     puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-
+    puts phrase
+    puts phrase
+    puts phrase
+    puts phrase
 end 
